c6acf9bcfed2a921d60d0d7a886779585ee95759
xlostcontrol <th.albertip@gmail.com>
2022-08-24 22:06:21 -0600

    script 2009

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function yearOf ()  {
     } else if(year >= 1995 && year <= 1999) {
         alert("late but still peak shoegaze")
     } else if (year >= 2000 && year <= 2009) {
-        alert("avant garde shoegaze, somewhere around here they invented the therm nugaze what even is nugaze i dont know")
+        alert("avant garde shoegaze, somewhere around here they invented 'nugaze' (what even is nugaze? i dont know)")
     } else if (year >= 2010 && year <= 2016) {
         alert("the rebirth of dreampop and shoegaze 2.0")
     } else if (year >= 2017 && year <= 2022) {
